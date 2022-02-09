6c3aada5ae59249f1e4c0b0923514d9d11f10840
Tim Nonet <tim.nonet@gmail.com>
2022-02-09 08:11:48 -0600

    Rename package directory and try python_example from PyBind11

---
+++ pypkg/tests/test_pybind_module.py
@@ -0,0 +1,13 @@
+#
+# def test_import():
+#     from sample_bindings import pybind_module
+#
+#
+# def test_import2():
+#     from sample_bindings import add, subtract
+#
+#
+# def test_pybind_module():
+#     from sample_bindings import add, subtract
+#     assert add(1, 1) == 2
+#     assert subtract(2, 1) == 1
